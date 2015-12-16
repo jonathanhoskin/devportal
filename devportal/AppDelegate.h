55a4b0b6efbb13f8a731c38f10225afe703804dd
Jonathan Hoskin <jonathan.hoskin@visfleet.com>
2015-12-16 15:30:48 +1300

    Initial Commit

---
+++ devportal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  devportal
+//
+//  Created by Jonathan Hoskin on 16/12/15.
+//  Copyright © 2015 Hoskin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ devportal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  devportal
+//
+//  Created by Jonathan Hoskin on 16/12/15.
+//  Copyright © 2015 Hoskin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
